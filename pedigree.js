d2cfe67371f08f7c3f6c17261624c80193b2368b
Fish Davidson <code@fishdavidson.com>
2018-12-26 17:53:11 -0600

    added death notifications and corrected randbetween function

--- pedigree.js
+++ pedigree.js
@@ -7,21 +7,25 @@ var ctxNuc = cNuc.getContext("2d");
 var numGenerations = 4; //How many generations the pedigree chart should span (must be greater than zero)
 var numParents = 2; //Generally leave this at 2 unless you have a weird race that requires more or fewer than two parents to give birth a new individual (must be integer >= 1)
 var box = {height: 75, width: 150, border: "black", fill: "white", posX: 0, posY: 0};
+var charStartAge = 18; //age at which Person 0 will be when the pedigree is generated
 var childDeathRate = 0.3;
 var connector = 50; //The width of the space between boxes of different generations
 var desiredYear = 0; //The year that the pedigree chart will "end" on
 var header = {height: 100, width: 100, isEnabled: true};
+var immigrationRate = 0.2;
 var lnameProp = 0; //controls how last names are propagated. 0 is patriarchal, 1 is matriarchal, 2+ is something else
 var maxAge = 100;
-var maxSiblings = 7;
+var maxAgeDiff = 10; //maximum age difference (in years) for individuals within a relationship
+var maxChildrenPerFam = 8;
 var marginBottom = 2;
 var marginLeft = 10;
 var marginRight = 10;
 var marginTop = 5;
-var pedigree = parentsArray([],generatePedigree());
 var sexualMaturityAge = 16;
 var vertSpacer = 25; //minimum vertical space between boxes of the same generation
 var year = 0; //origin year of the pedigree
+
+var pedigree = parentsArray([],generatePedigree()); //contains an array of person objects
 
 //set canvas dimensions based on the number of generations and box size
 c.width = (numGenerations * box.width) + (connector * (numGenerations - 1));
@@ -86,15 +90,15 @@ function createPerson (test) {
     var person = new Object();
     switch (test % 2) {
         case 0:
-            person.name = "Cheese";
+            person.fname = "Cheese";
             person.age = 18;
             break;
         case 1:
-            person.name = "Toast";
+            person.fname = "Toast";
             person.age = 21;
             break;
         default:
-            person.name = "Nope";
+            person.fname = "Nope";
             person.age = 65;
             break;
     }
@@ -103,6 +107,14 @@ function createPerson (test) {
 
 function causeOfDeath () {
     return deathIllness[Math.floor(Math.random() * (deathIllness.length))];
+}
+
+function checkDeath (person) {
+    console.log("Death check age: " + person.age + " " + person.isAlive + " " + person.lifespan);
+    if (person.age >= person.lifespan) {
+        person.isAlive = false;
+        console.log("Age: " + person.age + "Lifespan: " + person.lifespan);
+    }
 }
 
 function drawConnectors (parentX, parentY, childX, childY, color, width) {
@@ -243,7 +255,8 @@ function drawPedigree () {
 }
 
 function randBetween(a,b){
-    return a + Math.floor(Math.random() * b);
+    // the +1 is to make the range upper inclusive
+    return a + Math.floor(Math.random() * (b - a + 1));
 }
 
 function randomFirstName () {
@@ -262,40 +275,78 @@ function randomLastName () {
 	return lastNames[Math.floor(Math.random()*lastNames.length)];
 }
 
-function generateName () {
-    return randomFirstName() + " " + randomLastName();
+function setFullName (person) {
+    return person.fname + " " + person.lname;
 }
 
 function createPerson (child) {
     var person = new Object();
+    person.isAlive = true;
+    
 	switch (child) {
         case 0:
-            person.name = generateName();
-            person.age = randBetween(13, 23);
+            //var person = randGender();
+            //person.fname = generateName();
+            randGender(person);
+            person.lname = randomLastName();
+            person.age = charStartAge;
             break;
         default:
+            //var person = new Object();
             var parent_age_at_birth=randBetween(15,35);
-            person.name = generateName();
+            person.fname = randomFirstName();
+            person.lname = randomLastName();
             person.age = child.age + parent_age_at_birth;
+            person.lifespan = randBetween(sexualMaturityAge, maxAge);
+            console.log("Lifespan is " + person.lifespan);
             break;
     }
 	return person;	
 }
 
-function createMale () {
-    var person = new Object();
-    person.name = fnameM();
+function setMale (person) {
+    //var person = new Object();
+    person.fname = fnameM();
+    person.lname = "uncalculated";
     person.sex = "male";
     person.lifespan = randBetween(sexualMaturityAge, maxAge);
-    person.born = 1000;
+    person.born = 0;
+    //return person;
 }
 
-function createFemale () {
-    var person = new Object();
-    person.name = fnamef();
+function setFemale (person) {
+    //var person = new Object();
+    person.fname = fnameF();
+    person.lname = "uncalculated";
     person.sex = "female";
     person.lifespan = randBetween(sexualMaturityAge, maxAge);
-    person.born = 1000;
+    person.born = 0;
+    //return person;
+}
+
+function setOther (person) {
+    //var person = new Object();
+    person.fname = randomFirstName();
+    person.lname = "uncalculated";
+    person.sex = "other";
+    person.lifespan = randBetween(sexualMaturityAge, maxAge);
+    person.born = 0;
+    //return person;
+}
+
+function randGender (person) {
+    var i = randBetween(1, numParents);
+    switch (i) {
+        case 1:
+            setMale(person);
+            break;
+        case 2:
+            setFemale(person);
+            break;
+        default:
+            setOther(person);
+            break;
+    }
 }
 
 function generateParents(child, generationsLeft){
@@ -326,27 +377,27 @@ function generatePedigree () {
 
 function generatePedigree () {
     finalPerson=createPerson(0);
-    generateParents(finalPerson, numGenerations-1 );
+    generateParents(finalPerson, numGenerations - 1);
     return finalPerson;
 }
 
 //ezmac comments
 // I'm not doing great with functions and names but this works
 function parentsArray(current, person){
-    var arr=[];
-    var depth=numGenerations;
-    for( var i=0; i<=depth; i++){
+    var arr = [];
+    var depth = numGenerations;
+    for( var i = 0; i <= depth; i++){
         arr = arr.concat(BFRDescent(person, i));
     }
     return arr;
 }
 
 function BFRDescent(tree, depth){
-    var generation=[];
+    var generation= [];
     var arr = [tree];
     if (depth == 0) {return [tree];}
-    if (depth>0){
-        for (var j=0; j<tree.parents.length; j++) {
+    if (depth > 0){
+        for (var j = 0; j < tree.parents.length; j++) {
             generation = generation.concat(BFRDescent(tree.parents[j], depth-1));
         }
     }
@@ -369,7 +420,7 @@ function shuffleArray(array) {
 //test code
 
 for (i = 0; i <= pedigree.length - 1; i++) {
-    console.log("pedigree" + i + " is " + pedigree[i].name + " compared to 0, which is " + pedigree[0].name + " Age: " + pedigree[i].age);
+    console.log("pedigree" + i + " is " + pedigree[i].fname + " compared to 0, which is " + pedigree[0].fname + " Age: " + pedigree[i].age);
 }
 
 pedigree[0].ninja = "yes";
@@ -379,15 +430,24 @@ console.log(pedigree[0].ninja);
 
 drawPedigree();
 drawHeader();
-//drawProperty(pedigree[4].name, 50, 50);
+//drawProperty(pedigree[4].fname, 50, 50);
 
 for (var i = 0; i <= pedigree.length - 1; i++) {
-    drawProperty(pedigree[i].name, pedigree[i].cx + 10, pedigree[i].cy + 10);
+    checkDeath(pedigree[i]);
+    if (pedigree[i].isAlive == false) {
+        drawProperty(pedigree[i].fname + " " + pedigree[i].lname + " (D)", pedigree[i].cx + 10, pedigree[i].cy + 10);  
+    } else {
+        drawProperty(pedigree[i].fname + " " + pedigree[i].lname, pedigree[i].cx + 10, pedigree[i].cy + 10);   
+    }
 }
 
 for(var i = 1; i <= pedigree.length - 1; i++) {
     drawConnectors(pedigree[i].cx, pedigree[i].cy, pedigree[calcChild(i, numParents)].px, pedigree[calcChild(i, numParents)].py, "Black", 4);
     console.log("Child of " + i + " is " + calcChild(i, 2));
+}
+
+for (var i = 0; i <= 100; i++) {
+    console.log("Testing Randbetween(15,100)" + randBetween(15, 100));
 }
 
 /*ctx.font = "12px serif";
